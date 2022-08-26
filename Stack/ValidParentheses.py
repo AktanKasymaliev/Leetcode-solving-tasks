f67f76e346e608121e4de883c5a5e314fd711d46
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-08-27 00:43:37 +0600

    valid parentheses task done

--- Stack/ValidParentheses.py
+++ Stack/ValidParentheses.py
@@ -1,8 +1,40 @@
+from collections import deque
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
-    
+        """
+        Делаю pop из закрывающихся скобок стека,
+        и сравниваю с открывающимися скобками i
+        При таких кейсах  "((("
+        Они все добавляются в стек и потом pop'аются
+        в итоге если стек пустой это значит что надо False
+        """
+        stack = deque()
+
+        for i in s:
+            if i in "{[(":
+                stack.append(i)
+            elif i in "}])":
+                if not stack:
+                    return False
+
+                open_brack = stack.pop()
+                if open_brack == "(" and  i == ")":
+                    continue
+                elif open_brack == "[" and i == "]":
+                    continue
+                elif open_brack == "{" and i == "}":
+                    continue
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+            
+
+
 if __name__ == "__main__":
     s = Solution()
-    str_ = ""
+    str_ = "[["
     print(s.isValid(str_))

0211fe1b45cfe8ef62bf1be43f78cfd2d49c1f8b
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-08-27 13:54:08 +0600

    add gitignore

--- Stack/MinStack.py
+++ Stack/MinStack.py
@@ -1,8 +1,24 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
+class MinStack:
+
+    def __init__(self):
+        pass
+
+    def push(self, val: int) -> None:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def top(self) -> int:
+        pass
+
+    def getMin(self) -> int:
         pass
     
 if __name__ == "__main__":
-    s = Solution()
-    str_ = ""
-    print(s.isValid(str_))
+    val = ["MinStack","push","push","push","getMin","pop","top","getMin"]
+    obj = MinStack()
+    obj.push(val)
+    obj.pop()
+    param_3 = obj.top()
+    param_4 = obj.getMin()

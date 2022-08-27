954831f9aaf204ebd12db45e4da7c4464ac52d6d
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-08-27 22:38:31 +0600

    Min Stack solution found

--- Stack/MinStack.py
+++ Stack/MinStack.py
@@ -1,24 +1,40 @@
+from collections import deque
+
 class MinStack:
 
     def __init__(self):
-        pass
+        self.stack = deque()
+        self.minStack = deque()
 
     def push(self, val: int) -> None:
-        pass
+        self.stack.append(val)
+        if self.minStack:
+            self.minStack.append(
+                min(val, self.minStack[-1])
+                )
+        else:
+            self.minStack.append(val)
 
     def pop(self) -> None:
-        pass
+        self.stack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        pass
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        pass
+        return self.minStack[-1]
     
 if __name__ == "__main__":
-    val = ["MinStack","push","push","push","getMin","pop","top","getMin"]
+    actions =["MinStack","push","push","push","getMin","pop","top","getMin"]
+    values = [[],[-2],[0],[-3],[],[],[],[]]
     obj = MinStack()
-    obj.push(val)
+
+    obj.push([-2])
+    obj.push([0])
+    obj.push([-3])
+
+    print("Get min before: ", obj.getMin())
     obj.pop()
-    param_3 = obj.top()
-    param_4 = obj.getMin()
+    print("Top: ", obj.top())
+    print("Get min after: ", obj.getMin())

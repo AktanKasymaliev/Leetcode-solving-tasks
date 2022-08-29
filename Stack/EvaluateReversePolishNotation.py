bc99a70c8593803e6f6f9ea8e031b12063f0907e
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-08-29 21:04:15 +0600

    added Evaluate Reverse Polish Notation task from leetcode

--- Stack/EvaluateReversePolishNotation.py
+++ Stack/EvaluateReversePolishNotation.py
@@ -1,8 +1,33 @@
+from typing import List
+# O(n)
 class Solution:
-    def isValid(self, s: str) -> bool:
-        pass
-    
+    """
+    Если мы видим оператор, то это значит мы должны 
+    провести вычисления лево-стоящих цифр смотря на оператор
+    и удалить их из стека, но получившееся значение мы обратно 
+    добавляем в стек.
+    """
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []        
+        for t in tokens:
+            if t == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif t == "-":
+                #do act from left to right
+                r, l = stack.pop(), stack.pop()
+                stack.append(l - r)
+            elif t == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif t == "/":
+                #do act from left to right
+                r, l = stack.pop(), stack.pop()
+                stack.append(int(l / r))
+            else:
+                stack.append(int(t))
+
+        return stack.pop()
+
 if __name__ == "__main__":
     s = Solution()
-    str_ = ""
-    print(s.isValid(str_))
+    tokens = ["2","1","+","3","*"] #output: 9
+    print(s.evalRPN(tokens))

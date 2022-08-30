876ab2770eaaef7e2bcc0ca21b6ef8b247d857d7
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-08-30 19:17:05 +0600

    Daily Temperatures leetcode task has done

--- Stack/DailyTemperatures.py
+++ Stack/DailyTemperatures.py
@@ -1,8 +1,29 @@
+from typing import List
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        pass
+    """
+    Мы аппендим температуру если она меньше чем впереди стоящего
+    И stack пустой.
+    Иначе если стек не пустой и температура больше позади стоящего
+    Мы удаляем позади стоящего, и вычисляем расстояние между 
+    текущим значением и только что удалившего значения.
+    и так далее.
     
+    [75(4),71(2),69(1),72(3),76(0),73(0)]
+    """
+
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+
+        for indx, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stack_index = stack.pop()[-1]
+                res[stack_index] = (indx - stack_index)
+            stack.append([temp, indx])
+        return res
+
 if __name__ == "__main__":
     s = Solution()
-    str_ = ""
-    print(s.isValid(str_))
+    temp = [75,71,69,72,76,73]
+    print(s.dailyTemperatures(temp))
